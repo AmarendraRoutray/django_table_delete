b670467f563a0ad6b14ae5049d0ad1e0b3e6c5d5
AmarendraRoutray <amarendraroutray6@gmail.com>
2023-12-26 20:33:02 +0530

    first commit

---
+++ app2/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Country(models.Model):
+    country_name=models.CharField(max_length=100,primary_key=True)
+
+
+class Capital(models.Model):
+    country_name=models.ForeignKey(Country,on_delete=models.CASCADE)
+    cap_name=models.CharField(max_length=100)
